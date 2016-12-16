3647e23240a03dd3af5e143627a2219834b03c9d
Edward Moseley <etmoseley@yahoo.com>
2016-12-16 11:31:59 -0500

    Added URI Online Judge Folder

---
+++ URI Online Judge/areaOfACircle.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+/*
+*https://www.urionlinejudge.com.br/judge/en/problems/view/1002
+*/
+
+int main(){
+    int a;
+    float pi = 3.14159;
+    scanf("%d", &a);
+    printf("A=%.4f\n", a*a*pi);
+    return 0;
+}
+
+/*
+*Note: Only 60%. Difficult due to decimal/binary mathematics
+*/
---
+++ URI Online Judge/extremelyBasic.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+/*
+*https://www.urionlinejudge.com.br/judge/en/problems/view/1001
+*/
+
+int main(){
+    int a,b;
+    scanf("%d\n%d", &a, &b);
+    printf("X = %d\n", a + b);
+    return 0;
+}
